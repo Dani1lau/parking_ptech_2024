5fdecf10ea9f42a13086044db260381a113c2573
unknown <Aprendiz@BOGDFPCGMF4101.FormacionSena.RED>
2024-02-09 11:57:18 -0500

    Constructores sobrecargados

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -1,7 +1,33 @@
 package org.ptech081.parking;
 
+import org.ptech081.parking.entities.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // crear carros con los constructores:
+        Carro carrito1 = new Carro("BJC 598",
+                TipoVehiculo.CAMIONETA);
+        Carro carrito2 = new Carro();
+        carrito2.placa = "IUG 345";
+        carrito2.tipoVehiculo = TipoVehiculo.MOTO;
+
+        // crear clientes
+
+        Cliente cliente1 = new Cliente("Gilberta", "Uwu", 16746542l);
+
+        // invocar el metodo addCar :
+
+        cliente1.addCar(carrito2);
+        cliente1.addCar("ASD 156", TipoVehiculo.BUS);
+
+        // recorrer los del cliente
+
+        for (Carro c : cliente1.misCarros) {
+            System.out.println(c.placa + "" + c.tipoVehiculo);
+            System.out.println("-------------");
+
+        }
+
     }
 }
